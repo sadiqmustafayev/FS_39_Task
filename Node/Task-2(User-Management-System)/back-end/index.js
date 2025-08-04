e7aa2cd182d7fe728390b9ecaa27113e725f871e
shamilvasiyev <vasiyevsamil@gmail.com>
2025-08-04 20:26:42 +0400

    review added

--- Node/Task-2(User-Management-System)/back-end/index.js
+++ Node/Task-2(User-Management-System)/back-end/index.js
@@ -1,31 +1,38 @@
-import express from 'express';
-import { readFileSync, writeFileSync } from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import { readFileSync, writeFileSync } from "fs";
+import path from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const usersFilePath = path.join(__dirname, 'data', 'users.json');
+import { v4 } from "uuid";
+import cors from "cors";
+
+// import { fileURLToPath } from "url";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// const usersFilePath = path.join(__dirname, 'data', 'users.json');
+
+const usersFilePath = path.resolve("./data/users.json");
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }));
 
-let users = JSON.parse(readFileSync(usersFilePath, 'utf-8'));
+let users = JSON.parse(readFileSync(usersFilePath, "utf-8"));
 
-app.get('/api/users', (req, res) => {
+app.get("/api/users", (req, res) => {
   res.json(users);
 });
 
-app.post('/api/users', (req, res) => {
+app.post("/api/users", (req, res) => {
   const newUser = req.body;
   console.log("Yeni istifadəçi body:", newUser);
 
-  if (!newUser.name || !newUser.email) {
-    return res.status(400).json({ message: 'Name və Email mütləqdir' });
+  if (!newUser.name.trim() || !newUser.email.trim()) {
+    return res.status(400).json({ message: "Name və Email mütləqdir" });
   }
 
-
-  newUser.id = users.length + 1;
+  // newUser.id = `${users.length + 1}`;
+  newUser.id = v4();
   users.push(newUser);
 
   writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
@@ -33,11 +40,11 @@ app.post('/api/users', (req, res) => {
   res.status(201).json(newUser);
 });
 
-
 app.delete("/api/users/:id", (req, res) => {
-  const id = parseInt(req.params.id);
+  // const id = parseInt(req.params.id);
+  const userId = req.params.id;
 
-  const userIndex = users.findIndex(user => user.id === id);
+  const userIndex = users.findIndex((user) => user.id === userId);
 
   if (userIndex === -1) {
     return res.status(404).json({ message: "Istifadeci Tapilmadi" });
@@ -50,7 +57,6 @@ app.delete("/api/users/:id", (req, res) => {
   res.json({ message: "İstifadəçi silindi" });
 });
 
-
 app.listen(5000, () => {
-  console.log('✅ Server is running on port: 5000');
+  console.log("✅ Server is running on port: 5000");
 });
--- Node/Task-2(User-Management-System)/front-end/src/App.tsx
+++ Node/Task-2(User-Management-System)/front-end/src/App.tsx
@@ -1,7 +1,15 @@
 import { useEffect, useState } from "react";
 
+type UserType = {
+  id: string;
+  name: string;
+  email: string;
+  gender: string;
+  status: string;
+};
+
 const UserTable = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<UserType[]>([]);
 
   useEffect(() => {
     fetch("http://localhost:5000/api/users")
@@ -34,34 +42,34 @@ const UserTable = () => {
           </thead>
 
           <tbody>
-          {users.map((user, index) => (
-            <tr
-              key={user.id || index}
-              className="bg-gray-50 hover:bg-gray-100 transition"
-            >
-              <td className="px-4 py-2 border-b hover:text-green-600 cursor-pointer">
-                {index + 1}
-              </td>
-              <td className="px-4 py-2 border-b hover:text-green-600 cursor-pointer">
-                {user?.name || "N/A"}
-              </td>
-              <td className="px-4 py-2 border-b hover:text-green-600 cursor-pointer">
-                {user?.email || "N/A"}
-              </td>
-              <td className="px-4 py-2 border-b hover:text-green-600 cursor-pointer">
-                {user?.gender || "N/A"}
-              </td>
-              <td className="px-4 py-2 border-b hover:text-green-600 cursor-pointer">
-                {user?.status || "N/A"}
-              </td>
-              <td className="px-4 py-2 border-b">
-                <button className="text-blue-600 hover:underline">Edit</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-
-
+            {users.map((user, index) => (
+              <tr
+                key={user.id || index}
+                className="bg-gray-50 hover:bg-gray-100 transition"
+              >
+                <td className="px-4 py-2 border-b hover:text-green-600 cursor-pointer">
+                  {index + 1}
+                </td>
+                <td className="px-4 py-2 border-b hover:text-green-600 cursor-pointer">
+                  {user?.name || "N/A"}
+                </td>
+                <td className="px-4 py-2 border-b hover:text-green-600 cursor-pointer">
+                  {user?.email || "N/A"}
+                </td>
+                <td className="px-4 py-2 border-b hover:text-green-600 cursor-pointer">
+                  {user?.gender || "N/A"}
+                </td>
+                <td className="px-4 py-2 border-b hover:text-green-600 cursor-pointer">
+                  {user?.status || "N/A"}
+                </td>
+                <td className="px-4 py-2 border-b">
+                  <button className="text-blue-600 hover:underline">
+                    Edit
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>
