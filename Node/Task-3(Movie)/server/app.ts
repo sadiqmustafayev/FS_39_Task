476be406c923b95a2b184122552199c8320b4925
shamilvasiyev <vasiyevsamil@gmail.com>
2025-08-22 19:24:54 +0400

    fe setup added

--- Node/Task-3(Movie)/server/app.ts
+++ Node/Task-3(Movie)/server/app.ts
@@ -1,24 +1,25 @@
-import express from "express"
-import cors from "cors"
-import mongoose from "mongoose"
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
-mongoose.connect(process.env.DB_CONNECT_URL!).then(() => {
-  console.log("Db connected!");
-}).catch((err) => {console.log(err);
-})
+mongoose
+  .connect(process.env.DB_CONNECT_URL!)
+  .then(() => {
+    console.log("Db connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 console.log(PORT);
 
+const app = express();
 
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded())
-
+app.use(cors({ origin: [""] }));
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.listen(PORT, () => {
   console.log(`Server is running : ${PORT}`);
-})
+});
